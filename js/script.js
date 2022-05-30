369db1e84bada0b368da887ac225766578c0ef09
Valentin Meier <valentinmeier123@gmail.com>
2022-05-29 23:59:42 -0300

    mi primer ciclo de js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+debugger
+let repetir=parseInt(prompt(`ingresa cuantas veces del 1 al 100 quieres que "Hola" se repita`));
+
+let condicion = true;
+
+while (condicion) {
+    if (condicion==((repetir>=1) && (repetir<=100))) {
+        alert(`Felicidades, se mostraran ${repetir} holas en la consola`);
+        condicion=false;
+    }else {
+        repetir=parseInt(prompt(`ingresa nuevamente cuantas veces del 1 al 100 quieres que tu "Hola" se repita`));
+    }
+}
+for(let i= 1;i <= repetir; i++) {
+    console.log (`${i} Hola`)
+}
